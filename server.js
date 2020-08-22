647d9360da48d66a3750af5839c2dde734a44954
Roopa Madihally <roopabdr@gmail.com>
2020-08-22 16:54:10 +0530

    small changes

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const mongoose = require('mongoose');
 const Article = require('./models/article');
 const articleRouter = require('./routes/articles');
 const methodOverride = require('method-override');
+
+const cors = require('cors');
+
 const app = express();
 
-mongoose.connect('mongodb://localhost/blog', {
+mongoose.connect('mongodb+srv://admin:Wz0OY2jP5aUkger8@skill-curves-dev.hli7v.mongodb.net/blog?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
@@ -15,6 +18,8 @@ app.set('view engine', 'ejs'); // used to create HTML views automatically using 
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
+app.use(cors());
+
 
 app.get('/', async (req, res) => {
     const articles = await Article.find().sort({
