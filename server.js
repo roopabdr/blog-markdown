e4fb7510a6bf50977525a6b0d8d9ec6ddcd308fa
Roopa Madihally <roopabdr@gmail.com>
2020-09-03 10:58:01 +0530

    added login screens and passport authentication

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
 const express = require('express');
 const mongoose = require('mongoose');
 const Article = require('./models/article');
 const articleRouter = require('./routes/articles');
 const methodOverride = require('method-override');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
 
 const cors = require('cors');
+
+const initializePassport = require('./passport-config');
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+);
 
 const app = express();
 
@@ -16,18 +31,78 @@ mongoose.connect('mongodb+srv://admin:Wz0OY2jP5aUkger8@skill-curves-dev.hli7v.mo
 
 app.set('view engine', 'ejs'); // used to create HTML views automatically using EJS view engine
 
+const users = [];
+
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 app.use(cors());
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-
-app.get('/', async (req, res) => {
+app.get('/', checkAuthenticated, async (req, res) => {
     const articles = await Article.find().sort({
         createdAt: 'desc'
     });
-    res.render('articles/index', { articles: articles });
+    res.render('articles/index', { articles: articles, name: req.user.name });
+});
+
+app.get('/login', checkNotAuthenticated, (req,res) => {
+    res.render('login');
+});
+
+app.get('/register', checkNotAuthenticated, (req,res) => {
+    res.render('register');
+});
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}));
+
+app.post('/register', checkNotAuthenticated,  async (req,res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        });
+        res.redirect('/login');
+    } catch {
+        res.redirect('/register');
+    }
+    console.log(users);
+});
+
+app.delete('/logout', (req, res) => {
+    req.logOut();
+    return res.redirect('/login');
 });
 
 app.use('/articles',articleRouter);
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+
+    return res.redirect('/login');
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/');
+    }
+
+    next();
+}
 
 app.listen(5000);
